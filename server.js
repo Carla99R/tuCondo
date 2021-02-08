0419ebc14f9b707e02c3a7c83cb31a35cb0d7e31
mariajosediazdelgado <diaz.maria@correo.unimet.edu.ve>
2021-02-08 02:07:47 -0400

    con error desconocido en GreaphQL

--- server.js
+++ server.js
@@ -1,15 +1,46 @@
 const { ApolloServer } = require("apollo-server-express")
 const express = require("express")
-const {ApolloServer, qgl} = require("apollo-server-express")
+const {ApolloServer, gql} = require("apollo-server-express")
 
+// models
 const models = require('./backend/sequelize/models/index')
-
-// GraphQL
-
-
 
 models.sequelize.authenticate().then(() =>{ // conexion BD
     console.log("Conectado a la BD")
 })
 
 models.sequelize.sync() // sincroniza modelos con BD
+
+
+
+/// --------------
+
+// GraphQL  
+// typeDefs
+const typeDefs = gql`
+
+    type Query{
+        hello: String 
+    }
+
+`; // no segura si esta comilla
+
+// Resolvers
+const resolvers = { 
+
+    Query: {
+        hello: () => "Hello world"
+    }
+
+}
+
+const server = new ApolloServer( { typeDefs, resolvers, context: { models } } )
+const app = express();
+server.applyMiddleware( {app} );
+
+app.listen( {port: 4000}, () => {
+    console.log("Corriendo Servidor Apollo en http://localhost:4000" + server.graphqlPath)
+
+})// no estoy segura si colocar el 4000 asi decia la prepa
+
+
