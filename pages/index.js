74a322825c68b8d1c6b70a1e29a8ac58a566327c
Leonardo Gonzalez <59893051+Leopgf@users.noreply.github.com>
2021-02-09 19:29:31 -0400

    Esqueleto de la funcion para recuperar datos de la base de datos

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import styles from "../styles/landingPage.module.css";
 import Image from "next/image";
 import Navbar from "../components/navbar";
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client'; //npm install @apollo/client graphql
 
 const HomePage =({ Component, pageProps })=> {
+    console.log(pageProps)
     return(
         <>
             <div className={styles.orden}>
@@ -24,6 +26,37 @@ const HomePage =({ Component, pageProps })=> {
 
         </>
     )
+}
+
+// Video de donde saque esto https://youtu.be/oxUPXhZ1t9I
+export async function getStaticsProps() { //Función asincrona para consumir datos de la API
+
+    const client =  new ApolloClient({ // Cliente de Apolo
+        uri: `http://localhost:9000/graphql`,
+        cache: new InMemoryCache()
+    })
+
+    const { data } = await client.query({ // Query de graphql
+        query: gql`
+        query{
+            getUsuarios{
+              usuario_id
+              nombre
+              apellido
+              cedula
+              correo
+            }
+          }
+        `
+    })
+    console.log('////////////////////////')
+    console.log('data:', data)
+
+    return { // Retornar la data que trae el query
+        pageProps: {
+            usuarios: [] // Cambiar corchetes por la data cuando funcione
+        }
+    }
 }
 
 export default HomePage;
