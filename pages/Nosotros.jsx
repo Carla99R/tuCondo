f845d9b8b43d566e72c080f226377cbef005b0c9
mariajosediazdelgado <diaz.maria@correo.unimet.edu.ve>
2021-02-18 16:32:53 -0400

    nosotros page intento

--- pages/Nosotros.jsx
+++ pages/Nosotros.jsx
@@ -6,9 +6,26 @@ const Nosotros=()=>{
     return(
         <>
             <Navbar/>
-            <body>
-                <h1>Hola</h1>
-            </body>
+            
+            <div className={styles.orden}>
+                <figure className={styles.logo}>
+                    <Image src="/todos.jpg" alt="logo" className={styles.imagen}
+                           width={120}
+                           height={120}
+                    />
+                    
+                </figure>
+               
+                <h1 className = {styles.titulo}>Nosotros</h1>
+          
+                <Text className={styles.texto}>
+                    ¡ Bienvenid@! =D ~{"\n"}
+                    Aquí podrás registrar tus pagos y te haremos notificaciones desde tus gastos hasta de si van a cortar el agua ~{"\n"}
+                    En esta App nos adaptamos a las necesidades venezolanas de usuario.
+                </Text>
+                        
+            </div>
+
             {/* TODO Acá va la información de nosotros*/}
 
         </>
