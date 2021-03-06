670788d595d185090e4c3c316d286c60d6620efb
mariajosediazdelgado <diaz.maria@correo.unimet.edu.ve>
2021-03-06 17:00:33 -0400

    info intento listo

--- pages/Informacion.jsx
+++ pages/Informacion.jsx
@@ -6,8 +6,16 @@ const Informacion=()=>{
     return(
         <>
             <Navbar/>
-            <h1>Holisss</h1>
-            {/* TODO Acá va la información de nosotros*/}
+            <h1>Informacion</h1>
+            {
+                <p>
+                ¡Bienvenid@! =D 
+
+                Aquí podrás registrar tus pagos y te haremos notificaciones desde tus gastos hasta de si van a cortar el agua 
+
+                En esta App nos adaptamos a las necesidades venezolanas de usuario 
+                </p>
+            }
 
         </>
     )
--- pages/Nosotros.jsx
+++ pages/Nosotros.jsx
@@ -7,10 +7,12 @@ const Nosotros=()=>{
         <>
             <Navbar/>
             <body>
-                <h1>Hola</h1>
+                <h1>Nosotros</h1>
             </body>
-            {/* TODO Acá va la información de nosotros*/}
-
+            { 
+                <p>
+                </p> 
+            }
         </>
     )
 
